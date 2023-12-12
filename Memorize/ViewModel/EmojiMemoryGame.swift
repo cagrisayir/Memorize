380f9c1b74410648a1315e46b5b9f71cb374bb30
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-12 11:56:12 +0300

    created viewmodel = emojimemorygame

---
+++ Memorize/ViewModel/EmojiMemoryGame.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Omer Cagri Sayir on 12.12.2023.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemoryGame<String>
+    
+    init(model: MemoryGame<String>) {
+        self.model = model
+    }
+}
