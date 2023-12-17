49b5a91ff0660c5fbf8f0cb79793f6e908ce5362
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-17 15:02:18 +0300

    lecture 5

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -9,62 +9,59 @@ import SwiftUI
 
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
-    
+
     var body: some View {
         VStack {
             ScrollView {
                 cards
                     .animation(.default, value: viewModel.cards)
             }.scrollIndicators(.hidden)
-            Button("Shuffle"){
+            Button("Shuffle") {
                 viewModel.shuffle()
             }
         }
         .padding()
     }
-    
+
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)]) {
-            ForEach(viewModel.cards) {card in
+            ForEach(viewModel.cards) { card in
                 CardView(card)
-                    .aspectRatio(2/3, contentMode: .fit)
+                    .aspectRatio(2 / 3, contentMode: .fit)
                     .padding(4)
             }
         }
         .foregroundColor(.orange)
     }
-    
-    
-    
-    
 }
 
 struct CardView: View {
     let card: MemoryGame<String>.Card
-    
+
     init(_ card: MemoryGame<String>.Card) {
         self.card = card
     }
-    
+
     var body: some View {
         ZStack(alignment: .center) {
             let base = RoundedRectangle(cornerRadius: 12) // Type Inference
             Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                
+
                 Text(card.content)
                     .font(.system(size: 200))
                     .minimumScaleFactor(0.01)
                     .aspectRatio(1, contentMode: .fit)
             }
             .opacity(card.isFaceUp ? 1 : 0)
-            
+
             base
                 .fill()
                 .opacity(card.isFaceUp ? 0 : 1)
-        }    }
-    
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
 }
 
 #Preview {
